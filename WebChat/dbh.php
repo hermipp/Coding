9aa12d8ac20abb13a394823a359dda02e3b151c4
albertonl <43930380+albertonl@users.noreply.github.com>
2018-10-13 00:22:28 +0200

    Update dbh.php

--- WebChat/dbh.php
+++ WebChat/dbh.php
@@ -1,5 +1,5 @@
 <?php
-	$conn=mysqli_connect("localhost","albertonl","albertonl2005","mydatabase");
+	$conn=mysqli_connect("localhost","root","","mydatabase");
 	if(!$conn){
 		die("Connection failed".mysqli_connect_error());
 	}
