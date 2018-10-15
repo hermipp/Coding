48de22a6a04d73229ea45e320d06455a774ecd2e
albertonl <43930380+albertonl@users.noreply.github.com>
2018-10-15 10:55:38 +0200

    Update index-eng.php

--- ProjectManager/index-eng.php
+++ ProjectManager/index-eng.php
@@ -14,10 +14,11 @@
 				<tr><th>Username</th><td><input type="text" name="uname" placeholder="Username" required></td></tr>
 				<tr><th>Password</th><td><input type="password" name="psswd" placeholder="Password" required></td></tr>
 				<tr><td colspan="2" align="right"><input type="submit" name="login" value="Log in"></td></tr>
-				<tr><td colspan="2" align="center"><a href="http://localhost/projectmanager/index-esp.php">Ir a la versión en castellano</a></td></tr>
-				<tr><td colspan="2" align="center"><a href="http://localhost/projectmanager/index-val.php">Anar a la versió en valencià</a><br /></td></tr>
+				<tr><td colspan="2" align="center"><a href="index-esp.php">Ir a la versión en castellano</a></td></tr>
+				<tr><td colspan="2" align="center"><a href="index-val.php">Anar a la versió en valencià</a><br /></td></tr>
 				<tr><td colspan="2" align="center"><input type="checkbox" name="remember-eng" value="1"> <b>I prefer using English version</b></td></tr>
 				<tr><td colspan="2" align="center"><div class="alert alert-info alert-dismissable"><b>NOTE: if you select "I prefer using the English<br />version", if you log in in less than a month, you'll be </br>automatically redirected to the English version</b></div></td></tr>
+		<tr><td><input type="hidden" name="lang" value="eng"></td></tr>
 			</form>
 		</table>
 	</body>
