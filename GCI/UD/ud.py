b00cf77ae39856e3ed47b7ec84c73d8e722cd2c0
Alberto Navalon <alberto.navalonlillo@gmail.com>
2018-11-22 23:22:29 +0100

    Update ud.py (incomplete)

--- GCI/UD/ud.py
+++ GCI/UD/ud.py
@@ -11,7 +11,7 @@ began=False
 aux=0
 index=0
 
-while aux<strlen(text):
+while aux<len(text):
 	if text.find("^",aux)!=-1 and began==False:
 		start[index]=text.find("^",aux)
 		aux=(text.find("^",aux))+1
@@ -25,7 +25,29 @@ while aux<strlen(text):
 		MAX_INDEX = index
 		tagged.close()
 
+words = [dict() for i in range(MAX_INDEX+1)]
+curr_cat = 0
+cat = "c"
+
 # Main loop
 index=0
 while index<=MAX_INDEX:
-	#SEARCH FOR WORD (FROM '^' TO '<')
+	curr_word_index=start[index]
+	curr_word_end=end[index]
+	words[index].update({'w':""})
+
+	# GET WORD (FROM '^' TO '<')
+	while curr_word_index<curr_word_end:
+		if text.find("<",curr_word_index)==-1:
+			words[index]['w'].append(text[curr_word_index])
+			curr_word_index+=1
+		else:
+			break
+	while curr_word_index<curr_word_end
+		curr_cat+=1
+		cat.append(str(curr_cat))
+		words[index].update({cat:""})
+
+		while curr_word_index<curr_word_end and text.find(">",curr_word_index)==-1:
+			words[index][str(cat)].append(text[curr_word_index])
+			curr_word_index+=1
